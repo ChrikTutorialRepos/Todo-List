fd9049c12bbcf08a559e43d5cf03470459bca721
Chris Kelly <cgkelly@ucdavis.edu>
2019-01-22 09:43:02 -0800

    Complete Styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,23 @@
-alert("HELLO");
+$("ul").on("click",".delete",(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+}));
+
+$("ul").on("click","li",(function(){
+    $(this).toggleClass("checked");
+}));
+
+$("input").keypress(function(e){
+    if(e.which === 13){
+        var input = $(this).val();
+        $("ul").append($("<li><span class='delete'><i class='far fa-trash-alt'></i></span> " + input + "</li>"));
+        $(this).val("");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text'").fadeToggle();
+});
+
