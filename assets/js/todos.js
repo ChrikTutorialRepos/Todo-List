118d1b1fd138d5545891cabc994ef8b122959203
Chris Kelly <cgkelly@ucdavis.edu>
2019-01-23 10:40:13 -0800

    Add persistence to list of todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -21,3 +21,38 @@ $(".fa-plus").click(function(){
     $("input[type='text'").fadeToggle();
 });
 
+window.onbeforeunload = function(){
+    var lis = $("li");
+    var todos = [];
+    for(var i = 0; i < lis.length; i++){
+        if(lis[i].classList.contains("checked")){
+            todos.push(1 + "%%##%%");
+        }
+        else{
+            todos.push(0 + "%%##%%");
+        }
+        todos.push(lis[i].innerText + "%%##%%");
+    }
+    todos.push("");
+    window.localStorage.setItem("list", todos);
+};
+
+window.onload = function(){
+    var todosString = window.localStorage.getItem("list");
+    var todos = todosString.split("%%##%%,");
+    if(todosString.length === 0){
+        $("ul").append($("<li><span class='delete'><i class='far fa-trash-alt'></i></span>Create Todo List</li>"));
+    }
+    else{
+        for(var i = 0; i < todos.length-1; i+=2){
+            if(todos[i] == "0"){
+                $("ul").append($("<li><span class='delete'><i class='far fa-trash-alt'></i></span>" + todos[i+1] + "</li>"));
+            }
+            else{
+                $("ul").append($("<li class='checked'><span class='delete'><i class='far fa-trash-alt'></i></span>" + todos[i+1] + "</li>"));
+            }
+        }
+    }
+};
+
+
